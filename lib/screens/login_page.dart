5dd30e6324a6dc92984a87a27b5081da1e390d35
semedii <abdisamed2023@hotmail.com>
2024-10-29 20:31:53 +0300

    refactored loginpage

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,6 +1,5 @@
 import 'package:favmusic/components/loading_screen.dart';
 import 'package:favmusic/components/my_button.dart';
-import 'package:favmusic/components/my_text_field.dart';
 import 'package:favmusic/cubits/login_cubits/login_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
@@ -13,51 +12,53 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => _loginCubit,
-      child: BlocBuilder<LoginCubit, LoginState>(
-        builder: (context, state) {
-          if (state is LoginInitial) {
-            return Scaffold(
-              backgroundColor: Colors.white,
-              body: SingleChildScrollView(
-                child: Container(
-                  height: MediaQuery.of(context).size.height,
-                  child: Column(
-                    children: [
-                      SizedBox(height: 32.0),
-                      _getTitle(),
-                      _getLogo(),
-                      SizedBox(height: 10.0),
-                      _getSpotifyButton(context),
-                      Spacer(),
-                      _getSignupButton()
-                    ],
-                  ),
-                ),
-              ),
-            );
-          } else if (state is LoginSuccess) {
-            SchedulerBinding.instance.addPostFrameCallback((_) {
-              GoRouter.of(context).pushReplacement("/HomePage");
-            });
-          }
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: BlocProvider(
+        create: (context) => _loginCubit,
+        child: BlocBuilder<LoginCubit, LoginState>(builder: _mapStateToWidget),
+      ),
+    );
+  }
 
-          return const Center(child: LoadinScreen());
-        },
+  Widget _mapStateToWidget(BuildContext context, LoginState state) {
+    if (state is LoginInitial) {
+      return _buildLoginContent(context);
+    } else if (state is LoginSuccess) {
+      SchedulerBinding.instance.addPostFrameCallback((_) {
+        GoRouter.of(context).pushReplacement("/HomePage");
+      });
+    }
+
+    return const Center(child: LoadinScreen());
+  }
+
+  Padding _buildLoginContent(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 24),
+      child: Column(
+        children: [
+          _getTitle(),
+          _getLogo(),
+          _getSpotifyButton(context),
+        ],
       ),
     );
   }
 
   Widget _getTitle() {
-    return const Text(
+    return Text(
       "Welcome to FavMusic",
-      style: TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
-        color: Colors.blueGrey,
-        letterSpacing: 1.5,
-      ),
+      style: _getTitleTextStyle(),
+    );
+  }
+
+  TextStyle _getTitleTextStyle() {
+    return const TextStyle(
+      fontSize: 24.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.blueGrey,
+      letterSpacing: 1.5,
     );
   }
 
@@ -67,50 +68,10 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  Widget _getEmailField() {
-    return const MyTextField(
-      labelText: "Email",
-      prefixIconData: Icons.email,
-    );
-  }
-
-  Widget _getPassworfField() {
-    return const MyTextField(
-      labelText: "Password",
-      prefixIconData: Icons.lock,
-    );
-  }
-
-  Widget _getLoginButton() {
-    return MyButton(
-        onTap: () {
-          throw UnimplementedError();
-        },
-        text: "Log In");
-  }
-
   Widget _getSpotifyButton(BuildContext context) {
     return MyButton(
-      text: 'Login with Spotify',
-      color: const Color.fromARGB(255, 49, 51, 50),
-      onTap: () {
-        BlocProvider.of<LoginCubit>(context).authenticate();
-      },
-    );
-  }
-
-  Widget _getSignupButton() {
-    return TextButton(
-      onPressed: () {
-        throw UnimplementedError();
-      },
-      child: Text(
-        "Don't have an account? Sign up",
-        style: TextStyle(
-          color: Colors.grey[600],
-          fontSize: 16,
-        ),
-      ),
-    );
+        text: 'Login with Spotify',
+        color: const Color.fromARGB(255, 49, 51, 50),
+        onTap: BlocProvider.of<LoginCubit>(context).authenticate);
   }
 }
